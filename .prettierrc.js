4ac167f8b8bf4a5ace5721584e296298380fe784
Alain Guillon <zyrass@outlook.fr>
2023-09-02 22:17:17 +0200

    feat(ui): Mise en place de Husky
    Installation et mise en place de Husky, commitlint, .prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  //  Définit la largeur d'une tabulation à 2 espaces.
+  tabWidth: 2,
+
+  //  Ajoute automatiquement des points virgules à la fin des lignes.
+  semi: true,
+
+  // Utilise des guillemets simples (') pour les chaînes de caractères.
+  singleQuote: true,
+
+  // Ajoute une virgule finale dans les tableaux et les objets.
+  trailingComma: "all",
+
+  // Ajoute des parenthèses autour des paramètres d'une flèche (=>).
+  arrowParens: "always",
+
+  // Ajoute des espaces entre les accolades dans les objets.
+  bracketSpacing: true,
+};
---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  // Écrasement des règles selon une demande particulière.
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      ["core", "ui", "backend", "frontend", "devops"],
+    ],
+    "subject-case": [2, "always", "sentence-case"],
+    "body-max-line-length": [2, "always", 500],
+    "body-leading-blank": [2, "always"],
+    "scope-empty": [2, "never"],
+    "scope-case": [2, "always", "lower-case"],
+    "header-max-length": [2, "always", 50],
+    "header-full-stop": [2, "never", "."],
+    "body-case": [2, "always", "sentence-case"],
+    "my-custom-rule": [2, "always", "my-custom-pattern"],
+  },
+};
