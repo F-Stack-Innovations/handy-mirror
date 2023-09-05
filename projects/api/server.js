ab8d5c66a42a159eeab4a968c5f00b8040703357
Alain Guillon <zyrass@outlook.fr>
2023-09-05 18:51:48 +0200

    feat(backend): Dotenv installé sur l'API
    Mise en place du Dotenv avec la configuration de celui-ci. Ajout également des fichiers de configuration.

--- projects/api/server.js
+++ projects/api/server.js
@@ -1,9 +1,40 @@
+/**
+ * Author: GUILLON Alain
+ * version: 0.0.2
+ * Created_at: 05/09/2023
+ *
+ */
+
+/**
+ * =======================================================================
+ * Dépendances (modules)
+ * =======================================================================
+ */
+require('dotenv').config();
 const express = require('express');
 
+/**
+ * =======================================================================
+ * Configuration de l'application
+ * =======================================================================
+ */
 const app = express();
+
+// eslint-disable-next-line no-undef
+const HOST = process.env.HOST || 'localhost';
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000;
 
 app.get('*', (req, res) => {
   res.send('coucou');
 });
 
-app.listen(3000, () => console.log('server start'));
+/**
+ * =======================================================================
+ * Démarage de l'application selon un certain PORT.
+ * =======================================================================
+ */
+app.listen(PORT, () => {
+  console.log(`API démarré à cette adresse: http://${HOST}:${PORT}`);
+});
